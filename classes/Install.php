fbaf907e7783ab19b99036b4020c8fad79e5e0df
Vadim Sannikov <vvs@cdev.ru>
2011-07-03 22:27:02 +0400

    E:0039540 [*] The "LC_Connector" module is now compliant to the Drupal code standart

--- classes/Install.php
+++ classes/Install.php
@@ -8,7 +8,7 @@
  * @category  Litecommerce connector
  * @package   Litecommerce connector
  * @author    Creative Development LLC <info@cdev.ru>
- * @copyright Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
  * @link      http://www.litecommerce.com/
  * @since     1.0.0
@@ -20,6 +20,15 @@
  * @since 1.0.0
  */
 abstract class LCConnector_Install extends LCConnector_Abstract {
+
+    /**
+     * Database params (cache)
+     *
+     * @var   array
+     * @since 1.0.0
+     */
+    protected static $dbParams;
+
 
     // {{{ Hook handlers
 
@@ -76,10 +85,15 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
      * @since  1.0.0
      */
     public static function performInstall() {
-        $description = array('description' => t('LC class'), 'type' => 'varchar', 'length' => 255);
+        $description = array(
+            'description' => t('LC class'),
+            'type'        => 'varchar',
+            'length'      => 255,
+        );
+
         db_add_field('block_custom', 'lc_class', $description);
 
-        self::callSafely('Module', 'setDrupalRootURL', array(rtrim(lc_connector_detect_drupal_baseurl(), '/') . '/'));
+        self::callSafely('Module', 'setDrupalRootURL', array(self::getDrupalBaseURL()));
     }
 
     /**
@@ -92,70 +106,128 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
         db_drop_field('block_custom', 'lc_class');
     }
 
-    // }}}
-
-    // {{{ Check requirements
-
     /**
      * Check requirements
+     *
+     * @param string $phase Installation type
      *
      * @return array
      * @since  1.0.0
      */
     public static function checkRequirements($phase) {
-        $errorMsg = NULL;
-        $requirements = array();
-
-        // Trying to include LiteCommerce installation scripts
-        $errorMsg = self::includeLCFiles();
-
-        if (!isset($errorMsg)) {
-            $requirements = ('install' === $phase) ? self::checkRequirementsInstall() : self::checkRequirementsUpdate();
-
+        if (self::includeLCFiles()) {
+            $method = ('install' === $phase) ? 'Install' : 'Update';
+            $requirements = self::$method();
         }
-        elseif (defined('XLITE_INSTALL_MODE')) {
-            $requirements['lc_not_found'] = array('description' => $errorMsg, 'severity' => REQUIREMENT_ERROR);
+        else {
+            $requirements['lc_not_found'] = array(
+                'description' => st('LiteCommerce software not found in :dir', array('dir' => self::getLCDir())),
+                'severity'    => REQUIREMENT_ERROR,
+            );
         }
 
         return $requirements;
     }
 
+    // }}}
+
+    // {{{ Auxiliary methods
+
     /**
-     * Trying to include installation scripts and return an error message if failed
+     * Returns Drupal URL (original drupal_detect_baseurl() corrected to support aliases)
      *
      * @return string
      * @since  1.0.0
      */
-    protected static function includeLCFiles() {
-        $errorMsg = NULL;
+    public static function getDrupalBaseURL() {
+        $url = drupal_detect_baseurl();
 
-        if (!defined('XLITE_INSTALL_MODE')) {
-            define('XLITE_INSTALL_MODE', true);
+        if (preg_match('/\.php(.*)$/', basename($url))) {
+            $url = dirname($url);
         }
 
-        $includeFiles = array(
-            'Includes/install/init.php',
-            'Includes/install/install.php',
-        );
-
-        foreach ($includeFiles as $includeFile) {
-            $file = self::getLCCanonicalDir() . $includeFile;
-
-            if (file_exists($file)) {
-                require_once $file;
-
-            }
-            else {
-                $errorMsg = st(
-                    'LiteCommerce software not found in :lcdir (file :filename)',
-                    array(':lcdir' => self::getLCDir(), ':filename' => $file)
-                );
-                break;
-            }
+        if (!empty($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] !== $_SERVER['SERVER_NAME']) {
+            $url = str_replace(parse_url($url, PHP_URL_HOST), $_SERVER['HTTP_HOST'], $url);
         }
 
-        return $errorMsg;
+        return rtrim($url, '/') . '/';
     }
+
+    /**
+     * Trying to include installation scripts
+     *
+     * @return string
+     * @since  1.0.0
+     */
+    public static function includeLCFiles() {
+        if (!defined('XLITE_INSTALL_MODE')) {
+            define('XLITE_INSTALL_MODE', TRUE);
+        }
+
+        if (self::isLCExists()) {
+            $result = TRUE;
+
+            foreach (array('Includes/install/init.php', 'Includes/install/install.php')  as $includeFile) {
+                $file = self::getLCCanonicalDir() . $includeFile;
+
+                if (file_exists($file)) {
+                    require_once $file;
+                }
+                else {
+                    $result = FALSE;
+                    break;
+                }
+            }
+        }
+
+        return !empty($result);
+    }
+
+    /**
+     * Returns an array of database connection parameters
+     *
+     * @return array
+     * @since  1.0.0
+     */
+    public static function getDatabaseParams() {
+        if (!isset(self::$dbParams)) {
+            global $databases;
+
+            self::$dbParams = array();
+
+            if (!empty($databases['default']['default'])) {
+                $mysql = $databases['default']['default'];
+            }
+            elseif ('post' == drupal_strtolower($_SERVER['REQUEST_METHOD']) && !empty($_POST['mysql'])) {
+                $mysql = check_plain($_POST['mysql']);
+                $mysql['driver'] = 'mysql';
+            }
+
+            if (!empty($mysql)) {
+                self::$dbParams['mysqluser'] = $mysql['username'];
+                self::$dbParams['mysqlpass'] = $mysql['password'];
+                self::$dbParams['mysqlhost'] = $mysql['host'];
+                self::$dbParams['mysqlport'] = isset($mysql['port']) ? $mysql['port'] : '';
+                self::$dbParams['mysqlsock'] = isset($mysql['unix_socket']) ? $mysql['unix_socket'] : '';
+                self::$dbParams['mysqlbase'] = $mysql['database'];
+
+                if (isset($mysql['db_prefix'])) {
+                    self::$dbParams['drupal_prefix'] = $mysql['db_prefix'];
+                }
+                elseif (isset($mysql['prefix'])) {
+                    self::$dbParams['drupal_prefix'] = $mysql['prefix'];
+                }
+
+                self::$dbParams['driver'] = $mysql['driver'];
+            }
+        }
+
+        return self::$dbParams;
+    }
+
+    // }}}
+
+    // {{{ Check requirements
 
     /**
      * Check requirements in update mode ($phase != 'install')
@@ -166,12 +238,15 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
     protected static function checkRequirementsUpdate() {
         $requirements = array();
 
-        $message = 'The installed LiteCommerce software not found. '
-            . 'It is required to install LiteCommerce and specify correct path '
-            . 'to them in the LC Connector module settings.';
-
-        if (!isLiteCommerceInstalled(lc_connector_get_database_params(), $message)) {
-            $requirements['lc_not_installed'] = array('description' => st($message), 'severity' => REQUIREMENT_WARNING);
+        if (!isLiteCommerceInstalled(self::getDatabaseParams(), $message)) {
+            $requirements['lc_not_installed'] = array(
+                'description' => st(
+                    'The installed LiteCommerce software not found. '
+                    . 'It is required to install LiteCommerce and specify correct path '
+                    . 'to them in the LC Connector module settings.'
+                ),
+                'severity'    => REQUIREMENT_WARNING,
+            );
         }
 
         return $requirements;
@@ -185,8 +260,8 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
      */
     protected static function checkRequirementsInstall() {
         $requirements = array();
-        $dbParams = lc_connector_get_database_params();
-        $message = NULL;
+        $dbParams     = self::getDatabaseParams();
+        $message      = NULL;
 
         if (isLiteCommerceInstalled($dbParams, $message)) {
             $requirements['lc_already_installed'] = array(
@@ -195,7 +270,6 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
                 'description' => $message,
                 'severity'    => REQUIREMENT_WARNING,
             );
-
         }
         else {
             $stopChecking = FALSE;
@@ -222,7 +296,6 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
             }
 
             if (!$stopChecking) {
-
                 if (!defined('LC_URI')) {
                     define('LC_URI', preg_replace('/\/install(\.php).*$/', '', request_uri()) . '/modules/lc_connector/litecommerce');
                 }
@@ -241,21 +314,12 @@ abstract class LCConnector_Install extends LCConnector_Abstract {
                 $requirements = array_merge($requirements, doCheckRequirements());
 
                 foreach ($requirements as $reqName => $reqData) {
-                    $requirements[$reqName]['description'] = 'LiteCommerce: ' . $reqData[empty($reqData['description']) ? 'title' : 'description'];
+                    $requirements[$reqName]['description'] = 'LiteCommerce: '
+                        . $reqData[empty($reqData['description']) ? 'title' : 'description'];
 
-                    if (FALSE === $reqData['status']) {
-                        if (TRUE === $reqData['critical']) {
-                            $requirements[$reqName]['severity'] = REQUIREMENT_ERROR;
-
-                        }
-                        else {
-                            $requirements[$reqName]['severity'] = REQUIREMENT_WARNING;
-                        }
-
-                    }
-                    else {
-                        $requirements[$reqName]['severity'] = REQUIREMENT_OK;
-                    }
+                    $requirements[$reqName]['severity'] = $reqData['status']
+                        ? REQUIREMENT_OK
+                        : ($reqData['critical'] ? REQUIREMENT_ERROR : REQUIREMENT_WARNING);
                 }
             }
         }
--- js/block_manage.js
+++ js/block_manage.js
@@ -7,10 +7,9 @@
  * @category  Litecommerce connector
  * @package   Litecommerce connector
  * @author    Creative Development LLC <info@cdev.ru>
- * @copyright Copyright (c) 2010 Creative Development LLC <info@cdev.ru>. All rights reserved
+ * @copyright Copyright (c) 2011 Creative Development LLC <info@cdev.ru>. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
  * @link      http://www.litecommerce.com/
- * @see       ____file_see____
  * @since     1.0.0
  */
 
